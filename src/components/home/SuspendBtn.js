7838d914e5863bde3efb493b9a226493930e1b85
hl <786676631@qq.com>
2020-04-21 15:06:06 +0800

    增加分类，发布文章

--- src/components/home/SuspendBtn.js
+++ src/components/home/SuspendBtn.js
@@ -1,18 +1,38 @@
 import React from 'react';
 import './SuspendBtn.scss';
+import PropTypes from "prop-types"
+import {Button, OverlayTrigger, Popover} from 'react-bootstrap';
 
 class SuspendBtn extends React.Component {
+    static  propTypes = {
+        suspendBtn: PropTypes.func.isRequired,
+    };
+
     constructor(props) {
         super(props);
         this.state = {
             title: "开门"
         }
+        this.popoverLeft = (
+            <Popover id="popover-positioned-left" className={"leftPopover"} title="">
+                <Button onClick={this.addType}>新增分类</Button>
+                <Button onClick={this.publish}>发布文章</Button>
+            </Popover>
+        );
     }
+
+    addType = () => {
+
+    };
+
+    publish = () => {
+
+    };
 
     suspendBtn = () => {
         const {suspendBtn} = this.props;
         this.setState((preState, props) => {
-            return{
+            return {
                 title: preState.title == "开门" ? "关门" : "开门"
             }
         });
@@ -23,7 +43,9 @@ class SuspendBtn extends React.Component {
         const {title} = this.state;
         return (
             <div onClick={this.suspendBtn} className="suspendBtn">
-               <p className={"text"}>{title}</p>
+                <OverlayTrigger rootClose={true} trigger="click" placement="left" overlay={this.popoverLeft}>
+                    <Button className={"button"}>{title}</Button>
+                </OverlayTrigger>
             </div>
         );
     }
