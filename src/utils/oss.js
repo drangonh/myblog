8150b685692ffe321622a7c5840bc962f494bb12
786676631@qq.com <786676631@qq.com>
2020-05-24 18:28:45 +0800

    用户信息更新功能

--- src/utils/oss.js
+++ src/utils/oss.js
@@ -5,23 +5,6 @@
 import OSS from "ali-oss"
 import {get} from "../axios";
 
-// const client = new OSS({
-//     region: 'https://oss-cn-shanghai.aliyuncs.com',
-//     accessKeyId: 'LTAI4GGxeHkKA7j7iXdCPzwL',
-//     accessKeySecret: 'XATCi8qAiF5bLRU5cl9doGlPoz7V6T',
-//     bucket: 'blogdrag'
-// });
-//
-// // 设置文件地址
-// // 用户头像上传地址
-// const PhotoPath = "photo/"
-//
-// //文章分类图片上传地址
-// const LanguagePath = "language/"
-//
-// // 文章图片上传地址
-// const ArticlePath = "article/"
-
 /*方法说明
  *@method 方法名
  *@for 所属类名
@@ -31,15 +14,7 @@ import {get} from "../axios";
  * 实现将文件上传至当前Bucket或Bucket下的指定目录。实现将文件上传至当前Bucket或Bucket下的指定目录。
  *@return {返回值类型} 返回值说明
 */
-export async function uploadImage(data, file, func, err) {
-
-    // const client = new OSS({
-    //     region: 'oss-cn-shanghai',
-    //     accessKeyId: 'LTAI4G6gFSbu7JV7nvSkD4j3',
-    //     accessKeySecret: 'U8YEYlGYNxvcwA29Gae315kFaINDPT',
-    //     bucket: 'blogdrag'
-    // });
-
+export async function uploadImage(data, file, func, err, type) {
     let client
 
     let res = await get("oss/ossInfo", {})
@@ -53,7 +28,13 @@ export async function uploadImage(data, file, func, err) {
         });
     }
 
-    const PhotoPath = "photo/"
+    let PhotoPath = "photo/"
+
+    if (type == 2) {
+        PhotoPath = "language/"
+    } else if (type == 3) {
+        PhotoPath = "article/"
+    }
     // 支持File对象、Blob数据、以及OSS Buffer。
     // or const data = new Blob('content');
     // or const data = new OSS.Buffer('content'));
